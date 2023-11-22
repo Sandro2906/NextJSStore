756ad4d19e76c6984d7b7b4b3b6bd8413b0f86b1
Sandro Gataric <132219558+Sandro2906@users.noreply.github.com>
2023-11-22 17:03:19 +0100

    Update Wel.jsx

--- app/components/Wel.jsx
+++ app/components/Wel.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Wel = () => {
   return (
-    <div className='bg-purple-400 w-full h-auto flex flex-col justify-center items-center '>
+    <div className='bg-purple-400 w-full h-auto flex flex-col justify-center items-center px-4'>
     <h1 className='text-5xl p-4 text-white txtshdw'>WELCOME</h1>
     <p className='md:text-xl text-lg text-white txtshdw md:w-[750px] w-full h-auto  pb-4'>I created a website using Next.js, an API, and JSON. The main purpose of the site is to showcase my skills in that area of programming. I did not focus much on the design aspect. My goal was to create a functional site. The aesthetics were not my primary concern.</p>
 
